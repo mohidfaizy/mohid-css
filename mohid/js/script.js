4fbf37fb7af7e0c14ad5fc072ae9a1c56113b067
Mohid Faizy <104261879+mohidfaizy@users.noreply.github.com>
2022-09-05 09:37:34 +0500

    Jit Compiler Added in Mohid

---
+++ mohid/js/script.js
@@ -0,0 +1,41 @@
+/////////////////////////////
+////// JIT Compilation //////
+/////////////////////////////
+
+// Fetching all elements of document
+const jitEveryElement = document.body.getElementsByTagName("*");
+
+// Object to particular jit classes
+const jitAllowedProps = {
+  "color": "color",
+  "background": "bg",
+  "background-color": "bg-clr",
+  "background-image": "bg-img",
+  'font-size': "font-size",
+  "font-weight": "font-weight",
+};
+
+// Function to check jit element
+const jitCheck = () => {};
+
+// Loop to check every element
+for (let jitIndex = 0; jitIndex <= jitEveryElement.length; jitIndex++) {
+    jitTriggeredElement = jitEveryElement[jitIndex];
+    jitTriggeredElementClasses = jitTriggeredElement.classList;
+    jitTriggeredElementClasses.forEach((jitTriggered) => {
+        // Checking if any JIT class exists
+        Object.entries(jitAllowedProps).forEach((jitAllowedClass) => {
+            const [jitAllowedClassKey, jitAllowedClassValue] = jitAllowedClass;
+            const jitAllowedPropsPattern = new RegExp(jitAllowedClassValue, "g");
+            if(jitTriggered.match(jitAllowedPropsPattern)){
+                // Getting the value of particular JIT property value
+                let jitTriggeredElementClassValue = jitTriggered.match(/(\[)[# \w]+/g);
+                jitTriggeredElementClassValue = jitTriggeredElementClassValue[0].substr( 1, jitTriggeredElementClassValue[0].length);
+
+                console.log(jitAllowedClassKey,jitTriggeredElementClassValue);
+
+                jitTriggeredElement.style.setProperty(jitAllowedClassKey, jitTriggeredElementClassValue);
+            }
+        });
+    });
+}
